36a106978f99bd6fa61589277801f57c3ec2a06e
BelalBasal <basdibrahim@gmail.com>
2024-10-25 21:19:00 +0300

    first eddition

--- GetSecondMax/GetSecondMax.cpp
+++ GetSecondMax/GetSecondMax.cpp
@@ -2,7 +2,37 @@
 #include<algorithm>
 #include<assert.h>
 using namespace std;
+int GetSecondMax(int Numbers[],int Size)
+{
+	assert(Size >= 2);
+	int SecondMax = Numbers[1];
+	int Max = Numbers[0];
+	if (SecondMax > Max)
+		swap(SecondMax, Max);
+	
+		for (int i = 2;i < Size;i++)
+		{
+			if (Max < Numbers[i])
+			{
+				SecondMax = Max;
+				Max = Numbers[i];
+				
+				
+			}
+			
+			if (SecondMax > Max)
+				swap(SecondMax, Max);
+		}
+		return SecondMax;
+	
+	
+}
 int main()
 {
+	const int size = 5;
+	int numbers[size] = { 9,8,7,6,5 };
+	int SecondMax=GetSecondMax(numbers, size);
+	cout << SecondMax;
+
 }
 
